387709a67daf14b0fb230954cbcf1e01a5503133
HamidReza Montazer <montazerh82@gmail.com>
2025-05-29 17:13:30 +0330

    chore: update version

--- setup.py
+++ setup.py
@@ -2,12 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name="texttools",
-    version="0.0.8",
+    version="0.1.0",
     packages=find_packages(),
     author="Tohidi",
     description="set of my text tools",
-    install_requires=[
-        "openai==1.77.0"
-    ]
-    
+    install_requires=["openai==1.77.0"],
 )
