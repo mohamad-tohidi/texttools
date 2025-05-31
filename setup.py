ec4897879d9584b0ba3f3ea18ab70371cc02da30
Tohidi <the.mohammad.tohidi@gmail.com>
2025-05-31 10:43:01 +0330

    fix: merging conflict

--- setup.py
+++ setup.py
@@ -2,11 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="texttools",
-<<<<<<< HEAD
-    version="0.0.14",
-=======
     version="0.1.0",
->>>>>>> feature/add-gemma-style-codes
     packages=find_packages(),
     author="Tohidi",
     description="set of my text tools",
