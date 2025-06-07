9caca2a68c4c1b610069530c3dd112378ec79d1a
Tohidi <the.mohammad.tohidi@gmail.com>
2025-06-07 17:22:37 +0330

    fixed the reason format

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="texttools",
-    version="0.1.12",
+    version="0.1.13",
     packages=find_packages(),
     author="Tohidi",
     description="set of my text tools",
