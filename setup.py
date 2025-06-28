74909e249c73b948f796974e0a7800e7e61b51ae
Mohammad M. Givechi <mmg.pouya@gmail.com>
2025-06-28 16:36:01 +0330

    finalizing id_management branch merge

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="texttools",
-    version="0.1.20",
+    version="0.1.21",
     packages=find_packages(),
     author="Tohidi",
     description="set of my text tools",
--- texttools/__init__.py
+++ texttools/__init__.py
@@ -2,7 +2,7 @@ from texttools.tools.question_detector.llm_detector import LLMQuestionDetector
 from texttools.handlers import NoOpResultHandler, PrintResultHandler, ResultHandler, SaveToFileResultHandler
 from texttools.tools.categorizer.encoder_model.encoder_vectorizer import EmbeddingCategorizer
 from texttools.tools.categorizer.llm.openai_categorizer import LLMCategorizer
-from texttools.batch_manager import SimpleBatchManager
+from texttools.batch_manager import SimpleBatchManager, BatchJobRunner
 from texttools.tools.summarizer import LLMSummarizer
 
 
@@ -15,5 +15,6 @@ __all__ = [
     "EmbeddingCategorizer",
     "LLMCategorizer",
     "SimpleBatchManager",
+    "BatchJobRunner",
     "LLMSummarizer"
     ]
