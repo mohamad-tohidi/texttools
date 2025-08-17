ef53b7751c46457471f5afac9d4db387c23a0671
Tohidi <the.mohammad.tohidi@gmail.com>
2025-08-17 15:40:45 +0330

    #70 fix: import error

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="texttools",
-    version="0.1.40",
+    version="0.1.41",
     author="Tohidi",
     description="A set of high-level NLP tools",
     install_requires=["openai==1.97.1", "numpy==1.26.4"],
