bf73cd272c0e757f5dc618783712dd996c391739
Erfan Moosavi <erfanmoosavi84@gmail.com>
2025-08-24 13:19:24 +0330

    refactor: keyword_extractor #79

---
+++ texttools/tools/keyword_extractor/keyword_extractor.py
@@ -0,0 +1,35 @@
+from openai import OpenAI
+from pydantic import BaseModel
+
+from texttools.base_tool import BaseTool
+
+
+class Output(BaseModel):
+    keywords: list
+
+
+class KeywordExtractor(BaseTool):
+    """
+    Keyword extractor for Gemma-style models with optional reasoning step.
+    Outputs JSON with a single array field: {"keywords": ["keyword1", "keyword2", ...]}.
+    Allows optional extra instructions via `prompt_template`.
+    """
+
+    prompt_file = "keyword_extractor.yaml"
+    output_model = Output
+
+    def __init__(
+        self, client: OpenAI, *, model: str, use_reason: bool = False, **kwargs
+    ):
+        super().__init__(
+            client,
+            model=model,
+            use_reason=use_reason,
+            **kwargs,
+        )
+
+    def extract_keywords(self, text: str) -> list[str]:
+        parsed: Output = self.run(text)
+        result = {"keywords": parsed.keywords}
+        self._dispatch(result)
+        return result
---
+++ texttools/tools/keyword_extractor/test_keyword_extractor.py
@@ -0,0 +1,13 @@
+from openai import OpenAI
+
+from texttools.tools.keyword_extractor.keyword_extractor import KeywordExtractor
+
+client = OpenAI(
+    base_url="http://185.208.182.157:9310/v1", api_key="hamta_T0k3n879875412"
+)
+
+tool = KeywordExtractor(client=client, model="gemma-3", use_reason=False)
+c = tool.extract_keywords(
+    "سلام دوستان حالتون چطوره میخواستم یک نکته ای رو مطرح کنم و اون هم این هست که با توجه با شرایط پیش آمده، دوستان ما توانایی حضور و مشارکت در جلسه فردا را نخواهند داشت بدلیل اتفاقات ناگواری که برای خانواده های ایشان رخ داده است. خوشحالیم که شما را انسان هایی صبور میبینیم که این مسائل را درک میکنند."
+)
+print(c)
