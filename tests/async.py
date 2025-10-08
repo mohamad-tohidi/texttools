85c8386e21bf79ba4fa7a05d67a1b99851d28976
Erfan Moosavi <erfanmoosavi84@gmail.com>
2025-10-08 15:05:23 +0330

    fix: async test file

--- tests/async.py
+++ tests/async.py
@@ -2,20 +2,27 @@ import os
 import asyncio
 
 from dotenv import load_dotenv
-from openai import OpenAI
+from openai import AsyncOpenAI
 
 from texttools import AsyncTheTool
 
 load_dotenv()
 API_KEY = os.getenv("OPENAI_API_KEY")
 
-client = OpenAI(base_url="https://openrouter.ai/api/v1", api_key=API_KEY)
+client = AsyncOpenAI(base_url="https://openrouter.ai/api/v1", api_key=API_KEY)
 tool = AsyncTheTool(client=client, model="gpt-4o-mini")
 
 
 async def main():
-    result = await tool.categorize("سلام، حالت چطوره؟")
-    print(result["result"])
+    original = "چه کسی به عنوان اولین نفر وارد بهشت خواهد شد؟"
+    result1 = await tool.rewrite(original, mode="positive")
+    result2 = await tool.rewrite(original, mode="negative")
+    result3 = await tool.rewrite(original, mode="hard_negative")
+
+    print("Original:", original)
+    print("Positive rewrite:", result1["result"])
+    print("Negative rewrite:", result2["result"])
+    print("Hard negative rewrite:", result3["result"])
 
 
 if __name__ == "__main__":
