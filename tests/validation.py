990ffca184f52ede5ec66c6010d3d18ce66db3c0
Erfan Moosavi <erfanmoosavi84@gmail.com>
2025-11-06 21:01:57 +0330

    Update test files

--- tests/validation.py
+++ tests/validation.py
@@ -19,11 +19,22 @@ client = OpenAI(base_url=BASE_URL, api_key=API_KEY)
 t = TheTool(client=client, model=MODEL)
 
 
-# Define validator
-def validate(result: Any) -> bool:
+# Define categorizer validator
+def validate1(result: Any) -> bool:
     return "هیچکدام" not in result
 
 
+# Define text to question validator
+def validate2(result: Any) -> bool:
+    return "زندگی" not in result
+
+
 # Categorizer
-category = t.categorize("سلام حالت چطوره؟", validator=validate)
+category = t.categorize("سلام حالت چطوره؟", validator=validate1)
 print(category)
+
+print("-" * 40)
+
+# Question from Text Generator
+question = t.text_to_question("زندگی", validator=validate2, output_lang="Persian")
+print(question)
